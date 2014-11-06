123a6dda79c8fd60b366b5b8378df395f6dc494d
sneubig <neubig.stephen@gmail.com>
2014-11-06 17:16:03 -0500

    add backbone-on-rails gem that added the backbone files

---
+++ app/assets/javascripts/tv_show_guide.js
@@ -0,0 +1,13 @@
+window.TVShowGuide = {
+  Models: {},
+  Collections: {},
+  Views: {},
+  Routers: {},
+  initialize: function() {
+    alert('Hello from Backbone!');
+  }
+};
+
+$(document).ready(function(){
+  TVShowGuide.initialize();
+});
