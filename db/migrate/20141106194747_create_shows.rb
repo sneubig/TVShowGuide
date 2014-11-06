507350329e0cdec540f985234d6e67c7f072869b
sneubig <neubig.stephen@gmail.com>
2014-11-06 15:56:14 -0500

    migrate and seed database

--- db/migrate/20141106194747_create_shows.rb
+++ db/migrate/20141106194747_create_shows.rb
@@ -1,6 +1,13 @@
 class CreateShows < ActiveRecord::Migration
   def change
     create_table :shows do |t|
+    	t.string :name
+    	t.text :description
+    	t.string :image
+    	t.string :day
+    	t.string :time
+    	t.string :channel
+    	t.timestamps
     end
   end
 end
